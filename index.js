ad246e0b2f1f4e5b35ff281505f0e777b53a45bc
jmm <josemmazzei@gmail.com>
2020-01-04 04:13:06 -0300

    Fixed Regexs and not escaped strings in EJS

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+const path = require('path');
+
+app.set('port', process.env.PORT || 4000);
 app.use(express.static('public'));
+app.set('helpers', path.join(__dirname, '/helpers'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-   res.render('index');
-});
+app.use('/', require('./routes/routes'))
 
 app.listen(app.get('port'), ()=> {
     console.log(`server on port ${app.get('port')}`);
