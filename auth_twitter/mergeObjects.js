fba833816e012d18f08aee5ec148a71f74aca724
jmm <josemmazzei@gmail.com>
2020-01-28 11:21:45 -0300

    Refactor mergeObjs function

--- auth_twitter/mergeObjects.js
+++ auth_twitter/mergeObjects.js
@@ -1,8 +1,5 @@
 const mergeObjs = (obj1, obj2) => {
-    for (var attr in obj2) {
-        obj1[attr] = obj2[attr];
-    }
-    return obj1
+    return {...obj1, ...obj2}
 };
 
 module.exports = mergeObjs
