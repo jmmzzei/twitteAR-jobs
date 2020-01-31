fe425937c98adc626a0598cf48a4085d4b73a751
jmm <josemmazzei@gmail.com>
2020-01-31 20:42:22 -0300

    Implement error handling in request

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const request = require('request')
-const getAuthorization = require('../auth_twitter/auth')
+const getAuthorization = require('../auth_twitter/getAuthorization')
 const cardsGroupCreator = require('../helpers/cardsGroupCreator')
 
 const notWantedKeywords = '-Soy%20-estoy%20'
@@ -9,11 +9,10 @@ const wantedKeywords = 'TrabajoAr%20Buscamos%20'
 let QUERIES_AMOUNT = 100
 
 router.get('/', (req, res) => {
-
     let { authorization, url } = getAuthorization({ 'q': notWantedKeywords + wantedKeywords, 'count': QUERIES_AMOUNT, 'result_type': 'recent', 'tweet_mode': 'extended' });
 
     let config = {
-        url: url,
+        url,
         method: 'GET',
         headers: {
             'Authorization': authorization
@@ -21,11 +20,16 @@ router.get('/', (req, res) => {
         force_login: true,
         json: true
     }
+
     request(config, (err, resp, body) => {
-        QUERIES_AMOUNT = body.statuses.length
-        let bodyStatuses = body.statuses
-        let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
-        res.render('index', { cardsGroup })
+        if (err)
+            res.send('Something bad happened: ' + err)
+        else {
+            QUERIES_AMOUNT = body.statuses.length
+            let bodyStatuses = body.statuses
+            let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
+            res.render('index', { cardsGroup })
+        }
     })
 });
 
