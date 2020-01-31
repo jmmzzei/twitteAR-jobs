9e6a87403df3d195f55cfea1deaad33598883366
jmm <josemmazzei@gmail.com>
2020-01-31 19:45:55 -0300

    Implement oAuthSigningKey inside getAuthorization
    Change name of auth to getAuthorization to improve readability.
    oAuthSigningKey function was deleted and now is implemented inside
    getAuthorization because the task that the function was executing
    could be represented in one line inside the only file that is making
    use of it.

---
+++ auth_twitter/getAuthorization.js
@@ -0,0 +1,36 @@
+const oAuthBaseString = require('./oAuhtBaseString')
+const oAuthSignature = require('./oAuthSignature')
+
+module.exports = reqParams => {
+    
+    const   consumerKey = process.env.TWITTER_CONSUMER_KEY,
+            accessToken = process.env.TWITTER_ACCESS_TOKEN,
+            consumerSecret = process.env.TWITTER_CONSUMER_SECRET,
+            accessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+
+    let timestamp = Math.round(Date.now() / 1000)
+    let nonce = Buffer.from(consumerKey + ':' + timestamp).toString('base64')
+
+    const authBaseParams = {
+        oauth_consumer_key: consumerKey,
+        oauth_nonce: nonce,
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_timestamp: timestamp,
+        oauth_token: accessToken,
+        oauth_version: '1.0'
+    }
+
+    let { baseString, url } = oAuthBaseString(reqParams, authBaseParams)
+    let signingKey = consumerSecret + '&' + accessTokenSecret
+    let signature = oAuthSignature(baseString, signingKey)
+
+    return {authorization: 'OAuth ' +
+        'oauth_consumer_key="' + consumerKey + '", ' +
+        'oauth_nonce="' + nonce + '", ' +
+        'oauth_signature="' + signature + '", ' +
+        'oauth_signature_method="HMAC-SHA1", ' +
+        'oauth_timestamp="' + timestamp + '", ' +
+        'oauth_token="' + accessToken + '", ' +
+        'oauth_version="1.0"', 
+        url}
+}
--- auth_twitter/oAuthSigningKey.js
+++
@@ -1,5 +0,0 @@
-const oAuthSigningKey = (consumer_secret, token_secret) => {
-    return consumer_secret + '&' + token_secret;
-};
-
-module.exports = oAuthSigningKey
