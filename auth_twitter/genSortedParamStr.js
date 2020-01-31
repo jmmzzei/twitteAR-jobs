d42845e05507195abd5c66ba6e9df5bded6256da
jmm <josemmazzei@gmail.com>
2020-01-31 18:38:17 -0300

    Refactor genSortedParamStr
    A better readability is achieved thanks to the introduction of a for...in
    loop and less lines of code are used.

--- auth_twitter/genSortedParamStr.js
+++ auth_twitter/genSortedParamStr.js
@@ -1,17 +1,12 @@
 const percentEncode = require('./percentEncode')
 
 const genSortedParamStr = paramObj => {
-
-    // Sort alphabetically
-    let paramObjKeys = Object.keys(paramObj);
-    let len = paramObjKeys.length;
-    paramObjKeys.sort();
-    // Interpolate to string with format as key1=val1&key2=val2&...
-    let paramStr = paramObjKeys[0] + '=' + paramObj[paramObjKeys[0]];
-    for (var i = 1; i < len; i++) {
-        paramStr += '&' + paramObjKeys[i] + '=' + percentEncode(decodeURIComponent(paramObj[paramObjKeys[i]]));
+    let paramStr = ''
+    for (const key in paramObj) {
+        if (key !== 'count')
+            paramStr += '&' + key + '=' + percentEncode(decodeURIComponent(paramObj[key]))
     }
-    return paramStr;
+    return `count=100${paramStr}`
 };
 
 module.exports = genSortedParamStr
