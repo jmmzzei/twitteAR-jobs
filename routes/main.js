75b2d1be6cad729544fde476fc89e74086194440
jmm <josemmazzei@gmail.com>
2020-03-08 15:16:13 -0300

    Add /search endpoint

--- routes/main.js
+++ routes/main.js
@@ -9,28 +9,73 @@ const wantedKeywords = 'TrabajoAr%20Buscamos%20'
 let QUERIES_AMOUNT = 100
 
 router.get('/', (req, res) => {
-    let { authorization, url } = getAuthorization({ 'q': notWantedKeywords + wantedKeywords, 'count': QUERIES_AMOUNT, 'result_type': 'recent', 'tweet_mode': 'extended' });
 
-    let config = {
+  let { authorization, url } = getAuthorization({
+    q: notWantedKeywords + wantedKeywords,
+    count: QUERIES_AMOUNT,
+    result_type: 'recent',
+    tweet_mode: 'extended',
+  })
+
+  let config = {
+    url,
+    method: 'GET',
+    headers: {
+      Authorization: authorization,
+    },
+    force_login: true,
+    json: true,
+  }
+
+  request(config, (err, resp, body) => {
+    if (err) res.send('Something bad happened: ' + err)
+    else {
+      QUERIES_AMOUNT = body.statuses.length
+      let bodyStatuses = body.statuses
+      let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
+      res.render('index', { cardsGroup })
+    }
+  })
+
+
+
+})
+
+router.get('/search', (req, res) => {
+    
+    let countriesEncoded = percentEncode(req.query.country)
+    let hashtagsEncoded = percentEncode(req.query.hashtags)
+    let keywordsEncoded = percentEncode(req.query.keywords)
+
+    let { authorization, url } = getAuthorization({
+        q: notWantedKeywords + wantedKeywords,
+        count: QUERIES_AMOUNT,
+        result_type: 'recent',
+        tweet_mode: 'extended',
+      })
+    
+      let config = {
         url,
         method: 'GET',
         headers: {
-            'Authorization': authorization
+          Authorization: authorization,
         },
         force_login: true,
-        json: true
-    }
-
-    request(config, (err, resp, body) => {
-        if (err)
-            res.send('Something bad happened: ' + err)
+        json: true,
+      }
+    
+      request(config, (err, resp, body) => {
+        if (err) res.send('Something bad happened: ' + err)
         else {
-            QUERIES_AMOUNT = body.statuses.length
-            let bodyStatuses = body.statuses
-            let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
-            res.render('index', { cardsGroup })
+          QUERIES_AMOUNT = body.statuses.length
+          let bodyStatuses = body.statuses
+          let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
+          res.render('index', { cardsGroup })
         }
-    })
-});
+      })
 
-module.exports = router;
+})
+
+module.exports = router
+
+
