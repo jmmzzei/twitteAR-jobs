2abeb931cec8649a8a88a4d4cdd0aea7fd1736c7
jmm <josemmazzei@gmail.com>
2020-04-02 10:40:29 -0300

    Just to test

--- routes/main.js
+++ routes/main.js
@@ -16,38 +16,42 @@ router.use((req, res, next) => {
   next()
 })
 
+// router.get('/', (req, res) => {
+//   unwantedWord = req.query.unwanted + '%20'
+//   wantedKeywords = req.query.keywords
+//   hashtags = req.query.hashtags
+
+//   let { authorization, url } = getAuthorization({
+//     q: unwantedWord + wantedKeywords + hashtags,
+//     count: QUERIES_AMOUNT,
+//     result_type: 'recent',
+//     tweet_mode: 'extended',
+//   })
+
+//   let config = {
+//     url,
+//     method: 'GET',
+//     headers: {
+//       Authorization: authorization,
+//     },
+//     force_login: true,
+//     json: true,
+//   }
+
+//   console.log(url)
+//   request(config, (err, resp, body) => {
+//     if (err) res.send('Something bad happened: ' + err)
+//     else {
+//       QUERIES_AMOUNT = body.statuses.length
+//       let bodyStatuses = body.statuses
+//       let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
+//       res.render('index', { cardsGroup })
+//     }
+//   })
+// })
+
 router.get('/', (req, res) => {
-  unwantedWord = req.query.unwanted + '%20'
-  wantedKeywords = req.query.keywords
-  hashtags = req.query.hashtags
-
-  let { authorization, url } = getAuthorization({
-    q: unwantedWord + wantedKeywords + hashtags,
-    count: QUERIES_AMOUNT,
-    result_type: 'recent',
-    tweet_mode: 'extended',
-  })
-
-  let config = {
-    url,
-    method: 'GET',
-    headers: {
-      Authorization: authorization,
-    },
-    force_login: true,
-    json: true,
-  }
-
-  console.log(url)
-  request(config, (err, resp, body) => {
-    if (err) res.send('Something bad happened: ' + err)
-    else {
-      QUERIES_AMOUNT = body.statuses.length
-      let bodyStatuses = body.statuses
-      let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
-      res.render('index', { cardsGroup })
-    }
-  })
+  res.send('its all good man!')
 })
 
 module.exports = router
