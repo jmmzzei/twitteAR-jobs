a06f2ab1efc48654aa4e9038b71969d704b8a5ef
jmm <josemmazzei@gmail.com>
2020-04-04 12:27:23 -0300

    Change code format

--- auth_twitter/hmac_sha1.js
+++ auth_twitter/hmac_sha1.js
@@ -1,8 +1,8 @@
 const jsSHA = require('jssha')
 
 module.exports = hmac_sha1 = (string, secret) => {
-    let shaObj = new jsSHA("SHA-1", "TEXT")
-    shaObj.setHMACKey(secret, "TEXT")
-    shaObj.update(string)
-    return shaObj.getHMAC("B64")
+  let shaObj = new jsSHA('SHA-1', 'TEXT')
+  shaObj.setHMACKey(secret, 'TEXT')
+  shaObj.update(string)
+  return shaObj.getHMAC('B64')
 }
--- helpers/cardsGroupCreator.js
+++ helpers/cardsGroupCreator.js
@@ -1,14 +1,14 @@
 const cardContentCreator = require('./cardContentCreator')
 
 function cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT) {
-    const cardsContent = []
-    for (let i = 0; i < QUERIES_AMOUNT; i++) {
-        if (!/^RT/.test(bodyStatuses[i].full_text)) {
-            let cardData = cardContentCreator(bodyStatuses[i])
-            cardsContent.push(cardData)
-        }
+  const cardsContent = []
+  for (let i = 0; i < QUERIES_AMOUNT; i++) {
+    if (!/^RT/.test(bodyStatuses[i].full_text)) {
+      let cardData = cardContentCreator(bodyStatuses[i])
+      cardsContent.push(cardData)
     }
-    return cardsContent
+  }
+  return cardsContent.length > 0 ? cardsContent : ''
 }
 
 module.exports = cardsGroupCreator
