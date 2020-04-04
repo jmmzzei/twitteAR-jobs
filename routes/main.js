59cc7411903ffd897e99460b435cb6c5ecb7e6ec
jmm <josemmazzei@gmail.com>
2020-04-04 12:28:23 -0300

    Split the routes
    Split the main and the search routes. Now each one has its own controller.

--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const request = require('request')
-const getAuthorization = require('../auth_twitter/getAuthorization')
-const cardsGroupCreator = require('../helpers/cardsGroupCreator')
 
-let unwantedWord = ''
-let wantedKeywords = 'TrabajoAr%20Buscamos%20'
-let hashtags = ''
-let QUERIES_AMOUNT = 300
+let cardsGroup = ''
 
 router.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*'])
@@ -17,39 +11,7 @@ router.use((req, res, next) => {
 })
 
 router.get('/', (req, res) => {
-  unwantedWord = req.query.unwanted + '%20'
-  wantedKeywords = req.query.keywords
-  hashtags = req.query.hashtags
-  
-  let { authorization, url } = getAuthorization({
-    q: unwantedWord + wantedKeywords + hashtags,
-    count: QUERIES_AMOUNT,
-    result_type: 'recent',
-    tweet_mode: 'extended',
-  })
-  
-  let config = {
-    url,
-    method: 'GET',
-    headers: {
-      Authorization: authorization,
-    },
-    force_login: true,
-    json: true,
-  }
-  
-  console.log(url)
-  request(config, (err, resp, body) => {
-    if (err) res.send('Something bad happened: ' + err)
-    else {
-      QUERIES_AMOUNT = body.statuses.length
-      let bodyStatuses = body.statuses
-      let cardsGroup = cardsGroupCreator(bodyStatuses, QUERIES_AMOUNT)
-      res.render('index', { cardsGroup })
-    }
-  })
+  res.render('index', { cardsGroup })
 })
 
 module.exports = router
-
-
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 let dynamicIDHashtags = 0
 let data = ''
+console.log(1)
 
 function Label(id) {
   return document.getElementById(id)
@@ -10,26 +11,27 @@ function SearchSpan(input) {
   this.btn = document.createElement('button')
   this.input = input
 }
+console.log(2)
 
-SearchSpan.prototype.create = function() {
+SearchSpan.prototype.create = function () {
   this.btn.innerHTML = 'x'
   this.btn.setAttribute('id', ++dynamicIDHashtags)
   this.btn.className = 'span-btn'
 
   if (this.input.id === 'keywords') {
     this.span.className = 'keyw-span'
-  } else if(this.input.id === 'hashtags'){
+  } else if (this.input.id === 'hashtags') {
     this.span.className = 'hash-span'
   } else {
     this.span.className = 'unwanted-span'
   }
 
-  this.btn.addEventListener('click', e => {
+  this.btn.addEventListener('click', (e) => {
     e.preventDefault()
     let container = e.target.parentNode.parentNode
     let containerChilds = Array.from(container.childNodes)
 
-    containerChilds.forEach(element => {
+    containerChilds.forEach((element) => {
       if (
         element.childNodes.length > 1 &&
         element.childNodes[1].id === e.target.id
@@ -41,22 +43,23 @@ SearchSpan.prototype.create = function() {
 
   if (this.input.id === 'keywords') {
     this.span.innerText = this.input.value
-  } else if (this.input.id === 'hashtags'){
+  } else if (this.input.id === 'hashtags') {
     this.span.innerText =
       this.input.value[0] === '#'
         ? '' + this.input.value
         : '#' + this.input.value
   } else {
     this.span.innerText =
-    this.input.value[0] === '-'
-      ? '' + this.input.value
-      : '-' + this.input.value
+      this.input.value[0] === '-'
+        ? '' + this.input.value
+        : '-' + this.input.value
   }
+  console.log(3)
 
   this.span.appendChild(this.btn)
 }
 
-SearchSpan.prototype.renderIn = function(label) {
+SearchSpan.prototype.renderIn = function (label) {
   label.appendChild(this.span)
 }
 
@@ -65,15 +68,15 @@ function Input(id) {
   this.id = id
 }
 
-Input.prototype.getValue = function() {
+Input.prototype.getValue = function () {
   return this.input.value
 }
 
-Input.prototype.clear = function() {
+Input.prototype.clear = function () {
   this.input.value = ''
 }
 
-Input.prototype.hasSpaces = function() {
+Input.prototype.hasSpaces = function () {
   if (/\s/.test(this.input.value)) {
     return true
   } else {
@@ -81,8 +84,8 @@ Input.prototype.hasSpaces = function() {
   }
 }
 
-Input.prototype.listen = function() {
-  this.input.addEventListener('keypress', e => {
+Input.prototype.listen = function () {
+  this.input.addEventListener('keypress', (e) => {
     if (this.hasSpaces()) {
       let searchSpan = new SearchSpan(this.input)
       let label = new Label(`${this.id}-label`)
@@ -102,29 +105,30 @@ hashtagsInput.listen()
 let unwantedInput = new Input('unwanted')
 unwantedInput.listen()
 
-const submitBtn = document.getElementById('submit')
-submitBtn.addEventListener('click', e => {
+const submitBtn = document.getElementById('submitBtn')
+submitBtn.addEventListener('click', (e) => {
   e.preventDefault()
+
   let hashtags = [],
     keywords = [],
     unwanted = []
 
   let hashSpans = document.getElementsByClassName('hash-span')
-  Array.from(hashSpans).forEach(el => {
+  Array.from(hashSpans).forEach((el) => {
     hashtags.push(percentEncode(el.innerText.slice(1, -2)))
   })
 
   let keyWSpans = document.getElementsByClassName('keyw-span')
-  Array.from(keyWSpans).forEach(el => {
+  Array.from(keyWSpans).forEach((el) => {
     keywords.push(percentEncode(el.innerText.slice(0, -2)))
   })
 
   let unwantedSpans = document.getElementsByClassName('unwanted-span')
-  Array.from(unwantedSpans).forEach(el => {
+  Array.from(unwantedSpans).forEach((el) => {
     unwanted.push(percentEncode(el.innerText.slice(1, -2)))
   })
 
-  let url = new URL('https://5191d8f9.ngrok.io/'),
+  let url = new URL('http://localhost:3000/search/'),
     params = {
       hashtags:
         Array.isArray(hashtags) && hashtags.length
@@ -135,16 +139,18 @@ submitBtn.addEventListener('click', e => {
           ? keywords.join(' ') + ' ' + keywordsInput.getValue().trim() || ''
           : keywordsInput.getValue() || '',
       unwanted:
-      Array.isArray(unwanted) && unwanted.length
-        ? unwanted.join(' ') + ' ' + unwantedInput.getValue().trim() || ''
-        : unwantedInput.getValue() || '',
+        Array.isArray(unwanted) && unwanted.length
+          ? unwanted.join(' ') + ' ' + unwantedInput.getValue().trim() || ''
+          : unwantedInput.getValue() || '',
     }
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key]),
+  )
   window.location.href = url.href
 })
 
-const percentEncode = str =>
+const percentEncode = (str) =>
   encodeURIComponent(str).replace(
     /[!*()']/g,
-    char => '%' + char.charCodeAt(0).toString(16),
+    (char) => '%' + char.charCodeAt(0).toString(16),
   )
