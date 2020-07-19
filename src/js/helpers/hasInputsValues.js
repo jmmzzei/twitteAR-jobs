3faa4dc5b6259cce6e04da8299fe178cc9bd40a2
Jose Maria Mazzei <josemmazzei@gmail.com>
2020-07-19 11:08:52 -0300

    Refactor submitHandler
    hasInputsValues now receives an object with the inputs values.
    In submitHandler the condition of the second if was changed.

--- src/js/helpers/hasInputsValues.js
+++ src/js/helpers/hasInputsValues.js
@@ -1,9 +1,11 @@
-export const hasInputsValues = () => (
-        hashtagsInput.getValue() ||
-        unwantedInput.getValue() ||
-        keywordsInput.getValue() ||
-        unwanted.length > 0 ||
-        keywords.length > 0 ||
-        hashtags.length > 0
-)
-
+export const hasInputsValues = ({
+  hashtagsInput,
+  unwantedInput,
+  keywordsInput,
+}) =>
+  hashtagsInput.getValue() ||
+  unwantedInput.getValue() ||
+  keywordsInput.getValue() ||
+  unwanted.length > 0 ||
+  keywords.length > 0 ||
+  hashtags.length > 0
--- src/js/submitHandler.js
+++ src/js/submitHandler.js
@@ -1,26 +1,25 @@
-import { percentEncode } from './helpers/percentEncode'
 import { parseField } from './helpers/parseField'
 import { hasInputsValues } from './helpers/hasInputsValues'
 import { getSpans } from './helpers/getSpans'
 import { createWarning } from './helpers/createWarning'
 
 export const submitHandler = (hashtagsInput, keywordsInput, unwantedInput) => {
-    let { hashtags, keywords, unwanted } = getSpans()
+  let { hashtags, keywords, unwanted } = getSpans()
+  let inputs = { hashtagsInput, keywordsInput, unwantedInput }
 
-    if (hasInputsValues) {
-        let url = new URL('https://twittear-jobs.herokuapp.com/search/'),
-            params = {
-                hashtags: parseField(hashtags, hashtagsInput),
-                keywords: parseField(keywords, keywordsInput),
-                unwanted: parseField(unwanted, unwantedInput),
-            }
-        Object.keys(params).forEach(key =>
-            url.searchParams.append(key, params[key]))
-        window.location.href = url.href
-    } else {
-        if (!(document.getElementsByClassName('message').length > 0)) {
-            createWarning()
-        }
-    }
+  if (hasInputsValues(inputs)) {
+    let url = new URL('https://twittear-jobs.herokuapp.com/search/'),
+      params = {
+        hashtags: parseField(hashtags, hashtagsInput),
+        keywords: parseField(keywords, keywordsInput),
+        unwanted: parseField(unwanted, unwantedInput),
+      }
+    Object.keys(params).forEach(key =>
+      url.searchParams.append(key, params[key]),
+    )
+    window.location.href = url.href
+  } else {
+    if (document.getElementsByClassName('message').length == 0)
+      createWarning()
+  }
 }
-
